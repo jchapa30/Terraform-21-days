9dadeb53163e8a90648fa14758c2b4ae0321a91e
Joey Chapa <joeychapa@yahoo.com>
2023-07-15 15:19:56 -0500

    Configure remote state

--- provider.tf
+++ provider.tf
@@ -1,4 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "mynewtestbucket3030"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
+
